a982e44e9a670ba20c44008fdf0175516d30ee25
rifan <rifanripan@gmail.com>
2020-04-01 11:04:31 +0700

    Updates

---
+++ precache-manifest.9a8cb938337634e761627b33d49eeea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c70a4a1fc98c91dcc99fc8e674d810",
+    "url": "/rrifan/WEB3A-431741-Rifan-Nur-Cahyanto-REACT.git/index.html"
+  },
+  {
+    "revision": "ac273b3e77bc178fb6f6",
+    "url": "/rrifan/WEB3A-431741-Rifan-Nur-Cahyanto-REACT.git/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "7e5457705a39340ed7c5",
+    "url": "/rrifan/WEB3A-431741-Rifan-Nur-Cahyanto-REACT.git/static/css/main.091d8e90.chunk.css"
+  },
+  {
+    "revision": "ac273b3e77bc178fb6f6",
+    "url": "/rrifan/WEB3A-431741-Rifan-Nur-Cahyanto-REACT.git/static/js/2.fdfa79b8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/rrifan/WEB3A-431741-Rifan-Nur-Cahyanto-REACT.git/static/js/2.fdfa79b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5457705a39340ed7c5",
+    "url": "/rrifan/WEB3A-431741-Rifan-Nur-Cahyanto-REACT.git/static/js/main.9dbe0540.chunk.js"
+  },
+  {
+    "revision": "ba51505a84a0f5b3f2f8",
+    "url": "/rrifan/WEB3A-431741-Rifan-Nur-Cahyanto-REACT.git/static/js/runtime-main.c7713653.js"
+  }
+]);
